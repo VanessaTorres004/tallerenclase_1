37b57f7da0858391781189da11d328abb8d0acce
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-18 09:01:36 -0500

    cambio jugador

--- tallerenclase_1/Models/Jugadores.cs
+++ tallerenclase_1/Models/Jugadores.cs
@@ -1,12 +1,19 @@
-﻿namespace tallerenclase_1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tallerenclase_1.Models
 {
     public class Jugadores
     {
+        
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Nombre { get; set; }
+        
         public int Edad {  get; set; }
         public string Posicion {  get; set; }
-        public string Equipos { get; set; }
+        public Equipo Equipo { get; set; }
+        [ForeignKey("nombre")]
 
     }
 }
