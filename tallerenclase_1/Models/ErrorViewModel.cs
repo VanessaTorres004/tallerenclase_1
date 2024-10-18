4b99e14f732e708f172b1bfc5091a309094acb64
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-18 08:32:11 -0500

    Agregar archivos de proyecto.

---
+++ tallerenclase_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tallerenclase_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
