3dae0a2055fe24f03f333f90750aa1b580beda81
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-18 08:32:11 -0500

    Agregar archivos de proyecto.

---
+++ tallerenclase_1/Models/Jugadores.cs
@@ -0,0 +1,11 @@
+﻿namespace tallerenclase_1.Models
+{
+    public class Jugadores
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad {  get; set; }
+        public string Posicion {  get; set; }
+
+    }
+}
