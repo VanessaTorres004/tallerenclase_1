09f689f0322f34e268c179beb2004ffc47cf08b2
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-18 08:54:37 -0500

    estadio

---
+++ tallerenclase_1/Models/Estadio.cs
@@ -0,0 +1,10 @@
+﻿namespace tallerenclase_1.Models
+{
+    public class Estadio
+    {
+        public int Id { get; set; }
+        public string Dirreccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Capacidad { get; set; }
+    }
+}
